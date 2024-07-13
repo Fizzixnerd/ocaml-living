36dedecaa59bef42966e2070fce1b19fae46e4de
Matt Walker <matt.g.d.walker@gmail.com>
2024-07-13 19:53:05 -0400

    Add .mli for living_core.ml

--- lib/living_core.ml
+++ lib/living_core.ml
@@ -1,45 +1,24 @@
-(** A [dep] is a dependency of a value of type ['a t].*)
 type dep = Dep : 'a -> dep
 
-(** An ['a t] is an [unsafe_value: 'a] along with its
-    [dependencies: dep list].  In particular, the dependencies
-    cannot be garbage collected while this structure lives.  This
-    makes it suitable for encoding dependencies between, say, FFI
-    structures and pointers for the garbage collector. *)
 type 'a t = { unsafe_value: 'a; dependencies : dep list}
 
-(** [bind f x] returns the result of applying the function [f] to
-    [x.unsafe_value], adding to it the dependencies of [x] itself. *)
 let bind : ('a -> 'b t) -> 'a t -> 'b t =
   fun f x ->
     let y = f x.unsafe_value in
     { y with dependencies = Dep y :: x.dependencies @ y.dependencies}
 
-(** Inject an ['a] into an ['a t], whose only dependency is itself.*)
 let return : 'a -> 'a t =
   fun x -> { unsafe_value = x; dependencies = [Dep x]}
 
-(** [map f x] maps over the inner [x.unsafe_value] without modifying
-    its dependencies.  Note that since [bind], [return], and [(=>)] are 
-    the only safe ways of constructing an ['a t], that these dependencies
-    always include the never-mapped-over original [unsafe_value]. *)
 let map : ('a -> 'b) -> 'a t -> 'b t =
   fun f x -> { x with unsafe_value = f x.unsafe_value }
 
-(** [x => y] ensures that [y] lives at least as long as [x] does, by wrapping
-    [x] in a ['a t] and adding both [x] and [y] as dependencies. *)
 let (=>) x y = { unsafe_value = x; dependencies = [Dep x; Dep y]}
 
-(** [x ==> ys] ensures that the list of [ys] lives at least as long as [x] does, by
-    wrapping [x] in a ['a t] and adding [x] and the list of [ys] as dependencies. *)
 let (==>) x ys = {unsafe_value = x; dependencies = Dep x :: List.map (fun y -> Dep y) ys}
 
-(** See [bind]. *)
 let (>>=) x f = bind f x
 
-(** [keep_alive x] provides a weaker gaurantee than ['a t] in that [x] (and not 
-    things [x] depends on, if [x] is not of type ['a t]) is kept alive only up
-    the point where this function is called. *)
 let keep_alive x = ignore (Sys.opaque_identity x)
 
 module Let_syntax = struct
@@ -47,8 +26,6 @@ module Let_syntax = struct
 
   let (let+) x f = x |> map f
 
-  (** This is a simple binding operator for keeping the bound variable alive for
-      the whole time it is in scope.*)
   let (let$) x f =
     let ret = f x in
     keep_alive x;
---
+++ lib/living_core.mli
@@ -0,0 +1,51 @@
+(** A [dep] is a dependency of a value of type ['a t].*)
+type dep = Dep : 'a -> dep
+
+(** An ['a t] is an value of type ['a] along with its
+    dependencies.  In particular, the dependencies cannot be garbage collected while this structure lives.  This
+    makes it suitable for encoding dependencies between, say, FFI
+    structures and pointers for the garbage collector. *)
+type 'a t
+
+(** [bind f x] returns the result of applying the function [f] to
+    [x.unsafe_value], adding to it the dependencies of [x] itself. *)
+val bind : ('a -> 'b t) -> 'a t -> 'b t
+
+(** Inject an ['a] into an ['a t], whose only dependency is itself.*)
+val return : 'a -> 'a t
+
+(** [map f x] maps over the inner [x.unsafe_value] without modifying
+    its dependencies.  Note that since [bind], [return], and [(=>)] are 
+    the only safe ways of constructing an ['a t], that these dependencies
+    always include the never-mapped-over original [unsafe_value]. *)
+val map : ('a -> 'b) -> 'a t -> 'b t =
+
+(** [x => y] ensures that [y] lives at least as long as [x] does, by wrapping
+    [x] in a ['a t] and adding both [x] and [y] as dependencies. *)
+val (=>): 'a -> 'b -> 'a t
+
+(** [x ==> ys] ensures that the list of [ys] lives at least as long as [x] does, by
+    wrapping [x] in a ['a t] and adding [x] and the list of [ys] as dependencies. *)
+val (==>) 'a -> 'b list -> 'a t
+
+(** See [bind]. *)
+val (>>=) : ('a -> 'b t) -> 'a t -> 'b t
+
+(** [keep_alive x] provides a weaker guarantee than ['a t] in that [x] (and not 
+    things [x] depends on, if [x] is not of type ['a t]) is kept alive up the
+    point where this function is called. *)
+val keep_alive : 'a -> unit
+
+module Let_syntax = struct
+  let (let*) x f = x |> bind f
+
+  let (let+) x f = x |> map f
+
+  (** This is a simple binding operator for keeping the bound variable alive for
+      the whole time it is in scope.*)
+  let (let$) x f =
+    let ret = f x in
+    keep_alive x;
+    ret
+
+end
