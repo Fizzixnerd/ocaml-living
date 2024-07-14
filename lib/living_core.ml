92d69fde6da2f31daa2423d9b5f8348560ec6ddc
Matt Walker <matt.g.d.walker@gmail.com>
2024-07-13 22:40:20 -0400

    Add unsafe_free, named_return, and finaliser

--- lib/living_core.ml
+++ lib/living_core.ml
@@ -1,6 +1,30 @@
 type dep = Dep : 'a -> dep
 
-type 'a t = { unsafe_value: 'a; dependencies : dep list}
+type 'a t = { unsafe_value: 'a; dependencies : dep list; name: string option; mutable freed: bool}
+
+let error_msg ?name () =
+  Printf.sprintf
+    {|Living_core.t value (with name %s) is being garbage collected without 
+first being freed with Living_core.unsafe_free.  
+This can cause undefined behaviour, so the collection 
+of this object has been supressed.  If you really wanted it collected, 
+call Living_core.unsafe_free on it first.  Otherwise, this message will display
+and you will leak the memory of this value and all its dependencies.
+
+Hint: You can use Living_core.named_return to attempt to label this value for debugging
+purposes.|}
+    (name |> Option.value ~default:"<unnamed Living_core.t>")
+    
+let _global = ref { unsafe_value = (); dependencies = []; name = Some "_global"; freed = false }
+
+let construct ?name x deps =
+  let ret = {unsafe_value = x; dependencies = deps; name; freed = false} in
+  Gc.finalise (fun x ->
+    if not x.freed then
+      prerr_endline (error_msg ?name:x.name ());
+      _global := { !_global with dependencies = Dep x :: !_global.dependencies })
+    ret;
+  ret
 
 let bind : ('a -> 'b t) -> 'a t -> 'b t =
   fun f x ->
@@ -8,14 +32,21 @@ let bind : ('a -> 'b t) -> 'a t -> 'b t =
     { y with dependencies = Dep y :: x.dependencies @ y.dependencies}
 
 let return : 'a -> 'a t =
-  fun x -> { unsafe_value = x; dependencies = [Dep x]}
+  fun x -> construct x [Dep x]
+
+let named_return : string -> 'a -> 'a t =
+  fun name x -> construct ~name x [Dep x]
+
+let unsafe_free x =
+  x.freed <- true;
+  x.unsafe_value
 
 let map : ('a -> 'b) -> 'a t -> 'b t =
   fun f x -> { x with unsafe_value = f x.unsafe_value }
 
-let (=>) x y = { unsafe_value = x; dependencies = [Dep x; Dep y]}
+let (=>) x y = construct x [Dep x; Dep y]
 
-let (==>) x ys = {unsafe_value = x; dependencies = Dep x :: List.map (fun y -> Dep y) ys}
+let (==>) x ys = construct x (Dep x :: List.map (fun y -> Dep y) ys)
 
 let (>>=) x f = bind f x
 
--- lib/living_core.mli
+++ lib/living_core.mli
@@ -2,17 +2,35 @@
 type dep = Dep : 'a -> dep
 
 (** An ['a t] is an value of type ['a] along with its
-    dependencies.  In particular, the dependencies cannot be garbage collected while this structure lives.  This
-    makes it suitable for encoding dependencies between, say, FFI
-    structures and pointers for the garbage collector. *)
+    dependencies.  In particular, the dependencies cannot be garbage 
+    collected while this structure lives.  This makes it suitable for
+    encoding dependencies between, say, FFI structures and pointers 
+    for the garbage collector. *)
 type 'a t
 
 (** [bind f x] returns the result of applying the function [f] to
     [x.unsafe_value], adding to it the dependencies of [x] itself. *)
 val bind : ('a -> 'b t) -> 'a t -> 'b t
 
-(** Inject an ['a] into an ['a t], whose only dependency is itself.*)
+(** [return x] injects an ['a] into an ['a t], whose only dependency is itself.*)
 val return : 'a -> 'a t
+
+(** [named_return name x] injects an [x : 'a] into an ['a t], whose only dependency
+    is itself.  Provides an explicit [name : string] for help with debugging
+    erroneous garbage collections without a call to [unsafe_free].*)
+val named_return : string -> 'a -> 'a t
+
+(** [unsafe_free x] allows [x] to be garbage collected, and prevents the normal warning
+    that accompanies a ['a Living_core.t] being garbage collected.  Beware: this
+    is an unsafe operation that can lead to segfaults and bad data.  It is
+    recommended that you either bundle all structs and pointers that comprise the
+    ['a]'s true dependencies into another type (in the case that you need to 
+    maintain access to foreign data), or marshall all the data you want to OCaml
+    values directly (when you don't).
+    
+    Consider the use of this function an optimization that you should measure your
+    need for before you reach for it. *)
+val unsafe_free : 'a t -> 'a
 
 (** [map f x] maps over the inner [x.unsafe_value] without modifying
     its dependencies.  Note that since [bind], [return], and [(=>)] are 
@@ -29,22 +47,19 @@ val (=>): 'a -> 'b -> 'a t
 val (==>) : 'a -> 'b list -> 'a t
 
 (** See [bind]. *)
-val (>>=) : ('a -> 'b t) -> 'a t -> 'b t
+val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
 
 (** [keep_alive x] provides a weaker guarantee than ['a t] in that [x] (and not 
     things [x] depends on, if [x] is not of type ['a t]) is kept alive up the
     point where this function is called. *)
 val keep_alive : 'a -> unit
 
-module type LET_SYNTAX = sig
-  val (let*) : 'a t -> ('a -> 'b t) -> 'b t
+module Let_syntax : sig
+    val (let*) : 'a t -> ('a -> 'b t) -> 'b t
 
-  val (let+) : 'a t -> ('a -> 'b) -> 'b t
+    val (let+) : 'a t -> ('a -> 'b) -> 'b t
 
-  (** This is a simple binding operator for keeping the bound variable alive for
-      the whole time it is in scope.*)
-  val (let$) : 'a -> ('a -> 'b) -> 'b
-
+    (** This is a simple binding operator for keeping the bound variable alive for
+        the whole time it is in scope.*)
+    val (let$) : 'a -> ('a -> 'b) -> 'b
 end
-
-module Let_syntax : LET_SYNTAX
